779076fc87b102af4c00a19d6c4f0eeb904fd0e7
Catherine Waind <catherine.waind@bbc.co.uk>
2020-07-23 16:50:22 +0100

    pulling out credentials from request body

--- index.js
+++ index.js
@@ -2,17 +2,57 @@ const express = require("express");
 const pgp = require("pg-promise")();
 const Book = require("./book.js");
 const credentials = require("./credentials");
-
+const jwt = require("jsonwebtoken");
 const app = express();
+const bodyParser = require("body-parser");
 const port = 3000;
 
 const cn = {
   host: "localhost",
   port: 5432,
   database: "bookish",
-  user: credentials.username,
+  user: credentials.user,
   password: credentials.password,
   max: 30,
+};
+
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.any(
+    "SELECT * FROM member WHERE username = ${extractedUsername} AND password = ${extractedPassword}",
+    {
+      extractedUsername: username,
+      extractedPassword: password,
+    }
+  )
+    .then(function (data) {
+      res.send(data);
+      var login = data;
+
+      const listOfMembers = login.map((member) => {
+        return { username: member.username, password: member.password };
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+const createToken = () => {
+  return (token = jwt.sign(response.body, credentials.privateKey));
+};
+
+const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, credentials.privateKey);
+  } catch (e) {
+    console.log("e:", e);
+    return null;
+  }
 };
 
 const db = pgp(cn);
